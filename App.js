ba527c63603a674da5374f4b3c30d04e20619505
psharif <psharif@asu.edu>
2018-03-22 19:23:11 -0700

    Initial Files To Create Maps

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MapView style={styles.map}
+          region={{
+            latitude: 37.8715926,
+            longitude: -122.272747,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1
+          }}
+        >
+          <MapView.Marker 
+            coordinate={{
+              latitude: 37.8720355,
+              longitude: -122.27125799999999
+            }}
+            title={'Berkeley Extension FitQuest Yeah!'}
+            description={'It where the fittest Quest'}
+            />
+        </MapView>
       </View>
     );
   }
@@ -15,9 +30,19 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'absolute',
+    top: 0, 
+    left: 0, 
+    bottom: 0, 
+    right: 0, 
+    justifyContent: 'flex-end', 
+    alignItems: 'center'
   },
+  map:{
+    position: 'absolute',
+    top: 0, 
+    left: 0, 
+    bottom: 0, 
+    right: 0
+  }
 });

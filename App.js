225a9fa1ba15ee99b7b55223b064363d9db0288f
psharif <psharif@asu.edu>
2018-03-25 20:40:26 -0700

    Added Files for React-Native App

--- App.js
+++ App.js
@@ -1,77 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          region={{
-            latitude: 37.8715926,
-            longitude: -122.272747,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
-          }}
-        >
-
-          <Polyline
-            coordinates={[
-              { latitude: 37.8715259, longitude: -122.272747},
-              { latitude: 37.8835259, longitude: -122.262747},
-              { latitude: 37.8905209, longitude: -122.256168 },
-              { latitude: 37.8985259, longitude: -122.242747 },
-              { latitude: 37.9055259, longitude: -122.234727 },
-              { latitude: 37.9167342, longitude: -122.221004 }
-            ]}
-            strokeColor="#ee2525" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-              '#7F0000',
-              '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-              '#B24112',
-              '#E5845C',
-              '#238C23',
-              '#7F0000'
-            ]}
-            strokeWidth={3}
-          />
-
-          <MapView.Marker 
-            coordinate={{
-              latitude: 37.8720355,
-              longitude: -122.27125799999999
-            }}
-            title={'Berkeley Extension FitQuest Yeah!'}
-            description={'It where the fittest Quest'}
-            image={require("./assets/wizardHat.png")}
-            style="marker"
-            />
-        </MapView>
-      </View>
+      <MapView 
+        style={styles.container}
+         initialRegion={{
+            latitude: 37.872036,
+            longitude: -122.271258,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+         }}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0, 
-    left: 0, 
-    bottom: 0, 
-    right: 0, 
-    justifyContent: 'flex-end', 
-    alignItems: 'center'
+    height: '100%',
+    width: '100%'
   },
-  map:{
-    position: 'absolute',
-    top: 0, 
-    left: 0, 
-    bottom: 0, 
-    right: 0
-  },
-  marker:{
-    height: 10, 
-    width: 10
-  }
-
 });

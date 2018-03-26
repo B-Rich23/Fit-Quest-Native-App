645fc4e738463d41376682a63b74f83eec6d1da7
psharif <psharif@asu.edu>
2018-03-25 21:59:55 -0700

    Added CameraKit

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import { CameraKitCameraScreen } from 'react-native-camera-kit';
+
 import MapView from 'react-native-maps';
 
 const instructions = Platform.select({
@@ -21,18 +23,26 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+//const isCameraAuthorized = await CameraKitCamera.checkDeviceCameraAuthorizationStatus();
+//const isUserAuthorizedCamera = await CameraKitCamera.requestDeviceCameraAuthorization();
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <MapView 
-        style={styles.container}
-         initialRegion={{
-            latitude: 37.872036,
-            longitude: -122.271258,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-         }}
+      <CameraKitCameraScreen
+          actions={{ rightButtonText: 'Done', leftButtonText: 'Cancel' }}
+          onBottomButtonPressed={(event) => this.onBottomButtonPressed(event)}
+          scanBarcode={true}
+          laserColor={"blue"}
+          frameColor={"yellow"}
+
+          onReadQRCode={((event) => Alert.alert("Qr code found"))} //optional
+          hideControls={false}           //(default false) optional, hide buttons and additional controls on top and bottom of screen
+          showFrame={true}   //(default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
+          offsetForScannerFrame = {10}   //(default 30) optional, offset from left and right side of the screen
+          heightForScannerFrame = {300}  //(default 200) optional, change height of the scanner frame
+          colorForScannerFrame = {'red'} //(default white) optional, change colot of the scanner frame
       />
     );
   }
